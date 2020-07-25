6fd659b99bf623f60ed1bfca0f1bfaa3a9d20760
amiyaranjan.das <amiyaranjan.das@cbnits.com>
2020-07-25 12:13:16 +0530

    Updates

---
+++ precache-manifest.1f9b90cd1796e90d8c5fe04c8bcb4323.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "809b5091b1708d9e4bdce8bc654e384a",
+    "url": "/User-Management/index.html"
+  },
+  {
+    "revision": "31f57e0406eded5ee589",
+    "url": "/User-Management/static/css/2.d98961f1.chunk.css"
+  },
+  {
+    "revision": "37e8f24a1c6d73e2170d",
+    "url": "/User-Management/static/css/main.d47feb3a.chunk.css"
+  },
+  {
+    "revision": "31f57e0406eded5ee589",
+    "url": "/User-Management/static/js/2.651cd76f.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/User-Management/static/js/2.651cd76f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37e8f24a1c6d73e2170d",
+    "url": "/User-Management/static/js/main.00a165d6.chunk.js"
+  },
+  {
+    "revision": "d96e3c1bf69c8f81a646",
+    "url": "/User-Management/static/js/runtime-main.df0a39ed.js"
+  }
+]);
